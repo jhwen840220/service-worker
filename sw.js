e140c5930d902934ebb5416768cad3bec7d40964
ted.wen <ted.wen@ehsn.com.tw>
2020-07-24 18:17:12 +0800

    cache without add and re-fetch

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
+const CACHE_NAME = 'static-v1';
 // 當service worker在「安裝階段」時會觸發此事件
 self.addEventListener('install', function(event) {
     console.log('[Service Worker] Installing Service Worker ...', event);
     
     event.waitUntil(
         // cache a cat SVG
-        caches.open('static-v1')
-            .then(cache => cache.add('/cat.svg'))
+        caches.open(CACHE_NAME)
+            // .then(cache => cache.add('/'))
+            .then(self.skipWaiting())
     )
-     
-    self.skipWaiting();
 });
 
 // 當service worker在「激活階段」時會觸發此事件
@@ -19,9 +19,14 @@ self.addEventListener('activate', function(event) {
 
 
 self.addEventListener('fetch', event => {
-    const url = new URL(event.request.url);
-  
-    if (url.origin == location.origin && url.pathname == '/dog.svg') {
-      event.respondWith(caches.match('/cat.svg'));
-    }
+    console.log('[Service Worker] Fetch --> ', event.request.url);
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if(response) {
+                    return response;
+                }
+
+            })
+    )
 });
