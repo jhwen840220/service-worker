256f1d95a53745803e9ead5141c260d429cb5d28
ted.wen <ted.wen@ehsn.com.tw>
2020-07-24 17:09:07 +0800

    2. install & activate

--- sw.js
+++ sw.js
@@ -1,1 +1,27 @@
-console.log("??")
+// 當service worker在「安裝階段」時會觸發此事件
+self.addEventListener('install', function(event) {
+    console.log('[Service Worker] Installing Service Worker ...', event);
+    
+    event.waitUntil(
+        // cache a cat SVG
+        caches.open('static-v1')
+            .then(cache => cache.add('/cat.svg'))
+    )
+     
+    self.skipWaiting();
+});
+
+// 當service worker在「激活階段」時會觸發此事件
+self.addEventListener('activate', function(event) {
+    console.log('[Service Worker] Activating Service Worker ...', event);
+    clients.claim();
+});
+
+
+self.addEventListener('fetch', event => {
+    const url = new URL(event.request.url);
+  
+    if (url.origin == location.origin && url.pathname == '/dog.svg') {
+      event.respondWith(caches.match('/cat.svg'));
+    }
+});
