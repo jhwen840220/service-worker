c58efce5a19da0ad9b9458ef13063dbddf925fcb
ted.wen <ted.wen@ehsn.com.tw>
2020-07-27 16:02:46 +0800

    3.離線快取

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("test")
+fetch('http://localhost:3000/dog')
+    .then(res => res.json())
+    .then(data => {
+        document.getElementById("name").innerHTML = data.name;
+        document.getElementById("age").innerHTML = data.age;
+        document.getElementById("gender").innerHTML = data.gender;
+    });
--- sw.js
+++ sw.js
@@ -1,4 +1,12 @@
-const CACHE_NAME = 'static-v1';
+const CACHE_NAME = 'static-v2';
+const DYNAMIC_NAME = 'dynamic';
+const FILES_TO_CACHE = [
+	'/',
+    '/index.html',
+    '/index.js',
+    '/main.css',
+    '/horse.svg',
+];
 // 當service worker在「安裝階段」時會觸發此事件
 self.addEventListener('install', function(event) {
     console.log('[Service Worker] Installing Service Worker ...', event);
@@ -6,7 +14,7 @@ self.addEventListener('install', function(event) {
     event.waitUntil(
         // cache a cat SVG
         caches.open(CACHE_NAME)
-            // .then(cache => cache.add('/'))
+            .then(cache => cache.addAll(FILES_TO_CACHE))
             .then(self.skipWaiting())
     )
 });
@@ -14,19 +22,34 @@ self.addEventListener('install', function(event) {
 // 當service worker在「激活階段」時會觸發此事件
 self.addEventListener('activate', function(event) {
     console.log('[Service Worker] Activating Service Worker ...', event);
-    clients.claim();
+    
+    event.waitUntil(
+		//遍歷當前快取keys
+		caches.keys().then((cacheNames) => {
+		  return Promise.all(cacheNames.map((cacheName) => {
+			if (cacheName !== CACHE_NAME) {
+			  console.log('[ServiceWorker] Removing old cache', cacheName);
+			  return caches.delete(cacheName);
+			}
+		  }));
+		})
+		.then(() => self.clients.claim())
+	)
 });
 
 
 self.addEventListener('fetch', event => {
     console.log('[Service Worker] Fetch --> ', event.request.url);
+
     event.respondWith(
         caches.match(event.request)
             .then(function(response) {
                 if(response) {
                     return response;
                 }
-
+                else {
+                    return fetch(event.request);
+                }
             })
     )
 });
